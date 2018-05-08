ccd1b18f3dce971c902d91bb7bf4a1073b4bb9a5
Huan Wang wang.huan@rytong.com <wang.huan@rytong.com>
2018-05-08 22:36:14 +0800

    new file

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-05-08 22:32:48
+# @Author  : Wang Huan (wanghuan_ch@163.com)
+# @Link    : http://www.baidu.com
+# @Version : $Id$
+
+import os
+
+print("hello world")
